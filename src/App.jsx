e51bc72fe9c12cc842a7b4f07545247b494e61e3
imkashishkathuria <kathuriak805@gmail.com>
2025-08-09 18:06:48 +0530

    Days based on current UTC time and displayed table in UI

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ const App = () => {
   useEffect(() => {
     fetch("/data.json")
       .then(res => res.json())
-      .then(data => setData(data));
-  }, [])
+      .then(data => {
+        setData(data)
+      });
+  }, []);
+
 
   return (
     <div className='flex flex-col gap-5 items-center justify-center py-10'>
@@ -27,17 +30,32 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index} className='border-2 border-gray-700 text-[19px]'>
-              <td className='text-center border-r-2 border-gray-700 py-3'>{item.human_user}</td>
-              <td className='text-center border-r-2 border-gray-700'>{item.create_date}</td>
-              <td className='text-center border-r-2 border-gray-700'>{item.password_changed_date}</td>
-              <td className='text-center border-r-2 border-gray-700'>{item.days_since_last_password_changed}</td>
-              <td className='text-center border-r-2 border-gray-700'>{item.last_access_date}</td>
-              <td className='text-center border-r-2 border-gray-700'>{item.days_since_last_access}</td>
-              <td className='text-center border-r-2 border-gray-700 capitalize'>{item.mfa_enabled}</td>
-            </tr>
-          ))}
+          {data.map((item, index) => {
+            const today = new Date();
+            const nowUTC = Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate());
+            const passChangedDate = new Date(item.password_changed_date);
+            const lastAccessDate = new Date(item.last_access_date);
+            const diffAccessDays = Math.floor(
+              (nowUTC - Date.UTC(lastAccessDate.getUTCFullYear(), lastAccessDate.getUTCMonth(), lastAccessDate.getUTCDate())) / (1000 * 60 * 60 * 24)
+            );
+            const diffDays = Math.floor(
+              (nowUTC - Date.UTC(passChangedDate.getUTCFullYear(), passChangedDate.getUTCMonth(), passChangedDate.getUTCDate())) / (1000 * 60 * 60 * 24)
+            );
+
+            return (
+              <tr key={index} className='border-2 border-gray-700 text-[19px]'>
+                <td className='text-center border-r-2 border-gray-700 py-3'>{item.human_user}</td>
+                <td className='text-center border-r-2 border-gray-700'>{item.create_date}</td>
+                <td className='text-center border-r-2 border-gray-700'>{item.password_changed_date}</td>
+                <td className='text-center border-r-2 border-gray-700'>
+                  {diffDays}
+                </td>
+                <td className='text-center border-r-2 border-gray-700'>{item.last_access_date}</td>
+                <td className='text-center border-r-2 border-gray-700'>{diffAccessDays}</td>
+                <td className='text-center border-r-2 border-gray-700 capitalize'>{item.mfa_enabled}</td>
+              </tr>
+            );
+          })}
 
         </tbody>
       </table>
